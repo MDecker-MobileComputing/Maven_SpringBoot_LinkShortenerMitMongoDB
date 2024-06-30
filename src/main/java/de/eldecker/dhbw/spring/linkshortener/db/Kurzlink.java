422acc04fcfa8bd04f8298fac994942dad76f5de
M Decker <mide_42b@yahoo.de>
2024-06-30 19:50:52 +0000

    Mit# Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/de/eldecker/dhbw/spring/linkshortener/db/Kurzlink.java
@@ -0,0 +1,15 @@
+package de.eldecker.dhbw.spring.linkshortener;
+
+import java.net.URL;
+import java.time.LocalDateTime
+
+
+public class Kurzlink {
+
+  private String id;
+
+  private URL url;
+
+  private LocalDateTime erzeugt;
+
+}
