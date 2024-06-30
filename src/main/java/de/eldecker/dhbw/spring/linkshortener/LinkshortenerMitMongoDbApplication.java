340f7adc5a4c41aa0e43487e92f11401809a9ea4
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-06-30 21:00:26 +0200

    index.html + CSS dazu

--- src/main/java/de/eldecker/dhbw/spring/linkshortener/LinkshortenerMitMongoDbApplication.java
+++ src/main/java/de/eldecker/dhbw/spring/linkshortener/LinkshortenerMitMongoDbApplication.java
@@ -3,11 +3,16 @@ package de.eldecker.dhbw.spring.linkshortener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * Einstiegspunkt für Programm.
+ */
 @SpringBootApplication
 public class LinkshortenerMitMongoDbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LinkshortenerMitMongoDbApplication.class, args);
+	public static void main( String[] args ) {
+
+		SpringApplication.run( LinkshortenerMitMongoDbApplication.class, args );
 	}
 
 }
