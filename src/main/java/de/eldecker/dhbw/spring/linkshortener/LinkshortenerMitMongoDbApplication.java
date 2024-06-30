43c83a8ab8641eae30377f12f94f4b4ba42d62f5
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-06-30 20:57:13 +0200

    Projekt eingerichtet

---
+++ src/main/java/de/eldecker/dhbw/spring/linkshortener/LinkshortenerMitMongoDbApplication.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.linkshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkshortenerMitMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkshortenerMitMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/eldecker/dhbw/spring/linkshortener/LinkshortenerMitMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.linkshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkshortenerMitMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
